5fbb61d5bc3ed2e01759ee4f43f490a8831b9017
Xv1ous <xvious5@gmail.com>
2023-11-18 00:36:02 +0700

    nyobain html

--- elibrary/elabweb/views.py
+++ elibrary/elabweb/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the elabweb index.")
+    return render(request, 'home.html')
